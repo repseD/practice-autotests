d2874b630fb011b18e82f702c6272163acf5b1a7
Mikhaylov Artem <utb404@gmail.com>
2025-12-17 13:47:41 +0500

    review

--- my_project/clients/client.py
+++ my_project/clients/client.py
@@ -1,11 +1,21 @@
+# review: ожидалось использование библиотеки QAUtils,
+# в т.ч. важный момент, что мы испольузем внутри неё httpx вместо requests
+
+# review: в данном модуле собраны все запросы и он называется client.py, это ок,
+# но нужно разделять клиентов по функциональности: users_client.py, posts_client.py, ...
 import requests
 
 
 class UsersClient:
+    # review: гуд, что вынес base_url в переменную, но мы выносим их в .env,
+    # так как при масштабировании base_url будет использоваться во многих клиентах
     BASE_URL = "https://jsonplaceholder.typicode.com"
 
+    # review: т.к. блока логики нет, то в данном случае не хватает докстринга, описания того, что метод делает
     def get_users(self):
         return requests.get(f"{self.BASE_URL}/users")
-        
+
+    # review: важный момент, класс называется UsersClient, а здесь мы уже обращаемся к ручке,
+    # которая работает с постами, т.е. для этой ручки нужен был отдельный модуль posts_client.py
     def post_posts(self, data):
         return requests.post(f"{self.BASE_URL}/posts", json=data)
--- my_project/tests/test_client.py
+++ my_project/tests/test_client.py
@@ -1,22 +1,32 @@
 from my_project.clients.client import UsersClient
 
+# review: в двух тестовых методах ты инициализируешь клиента,
+# это дублирование кода и ненужное повторение действия для теста, по-хорошему создать здесь тестовый класс:
+# class TestClient: в котором ты один раз для всех тестов проинициализируешь клиента,
+# а дальше в тестах будешь через self обращаться к нему
 
+# review: отсутствуют аннотации allure как на тестовых методах, так и в шагах тестов
 def test_get_users():
     client = UsersClient()
     response = client.get_users()
 
+    # review: в QAUtils эта проверка встроена в механизм запросов, её не пришлось бы выполнять
     assert response.status_code == 200
 
     users = response.json()
     assert isinstance(users, list)
     assert len(users) > 0
 
+    # review: данные проверки - контрактное тестирование.
+    # Помимо того, что проверяется их наличие здесь же можно проверить ещё и их типы
     user = users[0]
     assert "id" in user
     assert "name" in user
     assert "username" in user
     assert "email" in user
 
+# review: в прошлом тесте шаги были выделены переходами на новую строку,
+# это увеличивало читаемость теста, в данном тесте их не стало
 def test_post_posts():
     client = UsersClient()
     data = {
